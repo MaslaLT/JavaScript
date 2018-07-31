1be42e15b3634c99bde4de0e38c626e6afa40273
MaslaLT <40176679+MaslaLT@users.noreply.github.com>
2018-07-31 16:07:14 +0300

    Js namu darbai 9 uzduotis atliktos

--- Namu darbas/script.js
+++ Namu darbas/script.js
@@ -17,7 +17,7 @@ function checkPrice() {
 
 //Antra uzduotis
 
-function isTeistumasChecked() {
+/*function isTeistumasChecked() {
     if(document.getElementById('teistas').checked == true || document.getElementById('neteistas').checked == true) {
         return true;
     } else {
@@ -45,4 +45,141 @@ function canGoToArmy() {
    } else {
         document.write('Jus esate teistas!! Tarnybai netinkamas.');
 }
+}
+*/
+
+//Trecia uzduotis
+
+/*
+function cristmasBonus() {
+ /*
+    let age = document.getElementById('age').value;
+
+    document.getElementById('bonusSize').innerHTML = " 0 pounds.";
+
+    if(age < 10 ) {
+        document.getElementById('bonusSize').innerHTML = " 50 pounds.";
+    } else if(age < 20) {
+        document.getElementById('bonusSize').innerHTML = " 100 pounds.";
+    } else {
+        document.getElementById('bonusSize').innerHTML = " 150 pounds.";
+    }
+}
+*/
+//Ketvirta uzduotis
+/*
+function lottery() {
+    let number = Math.floor(Math.random() * 15 + 1);
+console.log(number);
+    if(number <= 1) {
+        document.getElementById('lotteryPrize').innerHTML = " 3 tickets.";
+    } else if(number <= 3) {
+        document.getElementById('lotteryPrize').innerHTML = " 2 tickets.";
+    } else if(number <= 7) {
+        document.getElementById('lotteryPrize').innerHTML = " 1 tickets.";
+    } else {
+        document.getElementById('lotteryPrize').innerHTML = " LOST you are LOSSER!!!";
+    }
+}
+*/
+//Penkta uzduotis
+
+/*function lottery() {
+    let number = Math.floor(Math.random() * 15 + 1);
+
+    switch(number) {
+        case 1:
+            document.getElementById('lotteryPrize').innerHTML = " 3 tickets.";
+            break;
+        case 2:
+            document.getElementById('lotteryPrize').innerHTML = " 2 tickets.";
+            break;
+        case 3:
+            document.getElementById('lotteryPrize').innerHTML = " 2 tickets.";
+            break;
+        case 4:
+            document.getElementById('lotteryPrize').innerHTML = " 1 tickets.";
+            break;
+        case 5:
+            document.getElementById('lotteryPrize').innerHTML = " 1 tickets.";
+            break;
+        case 6:
+            document.getElementById('lotteryPrize').innerHTML = " 1 tickets.";
+            break;
+        case 7:
+            document.getElementById('lotteryPrize').innerHTML = " 1 tickets.";
+            break;
+        default:
+            document.getElementById('lotteryPrize').innerHTML = " LOST you are LOSSER!!!";
+            break;
+    }
+}
+*/
+//Sesta uzduotis
+
+/*function isThisYearLeap()
+{
+    let enterdYear = document.getElementById("enterdYear").value;
+
+    if(enterdYear % 4 != 0) {
+        document.getElementById('yearType').innerHTML = " Year is COMMON";
+    } else if(enterdYear % 100 != 0) {
+        document.getElementById('yearType').innerHTML = " Year is LEAP";
+    } else if(enterdYear % 400 != 0) {
+        document.getElementById('yearType').innerHTML = " Year is COMMON";
+    } else {
+        document.getElementById('yearType').innerHTML = " Year is LEAP";
+    }
+}
+*/
+
+//septinta uduotis
+/*
+let randomnumber = Math.floor(Math.random() * 5 + 1);
+
+function guesNumber()
+{
+    enteredNumber = document.getElementById('enteredNumber').value;
+console.log(randomnumber);
+    if(enteredNumber == randomnumber) {
+        document.getElementById('ansver').innerHTML = "Correct - well done!";
+    } else {
+        document.getElementById('ansver').innerHTML = "Wrong guess again.";
+    }
+}
+*/
+
+//Astunta uzduotis
+/*
+function celsiusToF()
+{
+    let enterdCelsiusRange = document.getElementById('enteredNumber').value;
+    let seperationIndex = enterdCelsiusRange.indexOf('-');
+    let rangeStart = enterdCelsiusRange.substr(0, seperationIndex);
+    let rangeEnd = enterdCelsiusRange.substr(seperationIndex + 1 , enterdCelsiusRange.length);
+    let parsedRangeStart = parseInt(rangeStart);
+    let parsedRangeEnd = parseInt(rangeEnd);
+
+    while(parsedRangeStart <= parsedRangeEnd) {
+        document.write(parsedRangeStart + "C = " + (parsedRangeStart * (9/5)+32) + " F" + "<br>");
+        parsedRangeStart++;
+    }
+}
+*/
+//Devinta uzduotis
+
+
+function starTriangle()
+{
+    let enteredNumber = document.getElementById('enteredNumber').value;
+    let star = "*";
+    let counter = 0;
+
+    do{
+        document.write(star);
+        document.write("<br>");
+        console.log(star);
+        star = star + "*";
+        counter++;
+    }while (counter < enteredNumber);
 }
